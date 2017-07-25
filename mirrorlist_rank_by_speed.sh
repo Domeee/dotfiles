cf7efa6b6e67096b9365c6272bbdab8e1b90645e
Dominique Huber <dominique@donhubi.ch>
2017-07-25 14:29:44 +0200

    Add mirrorlist rank update script

---
+++ mirrorlist_rank_by_speed.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+echo done

aaa536f9a09c6cb8dc479283dccef60741f7893f
Dominique Huber <dominique@donhubi.ch>
2018-09-02 08:36:46 +0200

    Update rank mirror list script

--- bin/mirrorlist_rank_by_speed.sh
+++ bin/mirrorlist_rank_by_speed.sh
@@ -2,4 +2,3 @@
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-echo done

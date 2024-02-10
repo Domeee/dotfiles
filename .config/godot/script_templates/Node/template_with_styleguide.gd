315b56a95368e7531c22874bf9568e67223fb08b
Dominique Huber <dominique@donhubi.ch>
2024-02-10 06:55:51 +0100

    Update godot script templates

--- .config/godot/script_templates/Node/template_with_styleguide.gd
+++ .config/godot/script_templates/Node/template_with_styleguide.gd
@@ -6,37 +6,50 @@ extends _BASE_
 ##
 ## Description
 
-# signals
+##########################################
+# SIGNALS
+##########################################
 
-# enums
+##########################################
+# ENUMS
+##########################################
 
-# constants
+##########################################
+# CONSTANTS
+##########################################
 
-# @export variables
+##########################################
+# @EXPORT VARIABLES
+##########################################
 
-# public variables
+##########################################
+# PUBLIC VARIABLES
+##########################################
 
-# private variables
+##########################################
+# PRIVATE VARIABLES
+##########################################
 
-# @onready variables
+##########################################
+# @ONREADY VARIABLES
+##########################################
 
 
-# optional built-in virtual _init method
+##########################################
+# LIFECYCLE
+##########################################
 func _init() -> void:
 	pass
 
 
-# optional built-in virtual _enter_tree() method
 func _enter_tree() -> void:
 	pass
 
 
-# built-in virtual _ready method
 func _ready() -> void:
 	pass
 
 
-# remaining built-in virtual methods
 func _process(delta: float) -> void:
 	pass
 
@@ -44,8 +57,14 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	pass
 
-# public methods
+##########################################
+# PUBLIC METHODS
+##########################################
 
-# private methods
+##########################################
+# PRIVATE METHODS
+##########################################
 
-# subclasses
+##########################################
+# SUBCLASSES
+##########################################

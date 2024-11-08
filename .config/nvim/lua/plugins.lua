085040bc8bca3c0fa5f4b5117bba96002126dd91
Dominique Huber <dominique@donhubi.ch>
2024-11-08 10:09:36 +0100

    Give GitHub copilot another try

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -64,6 +64,6 @@ require("chatgpt").setup({
 require("ibl").setup()
 require("nvim-surround").setup()
 
-vim.g.copilot_filetypes = {
-  ["*"] = false,
-}
+-- vim.g.copilot_filetypes = {
+--   ["*"] = false,
+-- }
